0b5591ee860bcbf1d3b0bfa66086764aba5f06ee
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2024-01-07 23:31:30 +0900

    [이미지 검색앱] go_router 수정

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:image_search_day50/data/repository/pixabay_repository.dart';
-import 'package:image_search_day50/domain/model/photo.dart';
-import 'package:image_search_day50/domain/use_case/get_top_five_most_view_use_case.dart';
-import 'package:image_search_day50/presentation/detail/detail_screen.dart';
-import 'package:image_search_day50/presentation/main/main_screen.dart';
-import 'package:image_search_day50/presentation/main/main_view_model.dart';
 import 'package:image_search_day51/data/repository/pixabay_repository.dart';
 import 'package:image_search_day51/domain/model/photo.dart';
 import 'package:image_search_day51/domain/use_case/get_top_five_most_viewd_use_case.dart';
