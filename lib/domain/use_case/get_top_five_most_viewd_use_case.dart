a54fc7fe98c6cb2d6d1dada73d42983472df2fde
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2024-01-13 23:58:42 +0900

    [이미지 검색앱] use_case중에 조회수가 가장 많은 순서로 수정작업완료

--- lib/domain/use_case/get_top_five_most_viewd_use_case.dart
+++ lib/domain/use_case/get_top_five_most_viewd_use_case.dart
@@ -1,14 +1,15 @@
 import 'package:image_search_day51/domain/model/photo.dart';
 import 'package:image_search_day51/domain/repository/photo_repository.dart';
 
-class GetTopFiveMostViewdImagesUseCase {
+class GetTopFiveMostViewedImagesUseCase {
   final PhotoRepository _repository;
 
-  GetTopFiveMostViewdImagesUseCase(this._repository);
+  GetTopFiveMostViewedImagesUseCase(this._repository);
+
   Future<List<Photo>> execute(String query) async {
     final photos = await _repository.getPhotos(query);
 
     photos.sort((a, b) => -a.views.compareTo(b.views));
-    return photos.take(10).toList();
+    return photos.take(5).toList();
   }
 }
