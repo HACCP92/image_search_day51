50b82c46689fbac6c0cec087eeb33d1c743f2d67
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2024-01-14 23:50:34 +0900

    [이미지 검색앱] take적용으로 이미지 숫자 설정

--- lib/domain/use_case/get_top_five_most_viewd_use_case.dart
+++ lib/domain/use_case/get_top_five_most_viewd_use_case.dart
@@ -1,15 +1,15 @@
 import 'package:image_search_day51/domain/model/photo.dart';
 import 'package:image_search_day51/domain/repository/photo_repository.dart';
 
-class GetTopFiveMostViewedImagesUseCase {
+class GetTopTwentyMostViewedImagesUseCase {
   final PhotoRepository _repository;
 
-  GetTopFiveMostViewedImagesUseCase(this._repository);
+  GetTopTwentyMostViewedImagesUseCase(this._repository);
 
   Future<List<Photo>> execute(String query) async {
     final photos = await _repository.getPhotos(query);
 
     photos.sort((a, b) => -a.views.compareTo(b.views));
-    return photos.take(5).toList();
+    return photos.take(20).toList();
   }
 }
